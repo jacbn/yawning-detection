eda5dde4d07438990cebb2fb7da781a6a35b37a7
jacbn <jabrown405@gmail.com>
2022-12-11 15:31:34 +0000

    added type annotations

--- yawnn/lib/eimuLearner.py
+++ yawnn/lib/eimuLearner.py
@@ -9,13 +9,8 @@ from tensorflow.keras.layers import Dense, LSTM, Dropout
 trainPercent = 0.8
 timestampPredicate = lambda tList: sum(map(lambda t: t.type == 'yawn', tList))
 
-model = Sequential()
-model.add(LSTM(units=10, return_sequences=True))
-model.add(Dense(units=1, activation='sigmoid'))
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
 # convert a single eimu file to a tuple of (trainX, trainY), (testX, testY)
-def eimuToLSTMInput(eimuPath, shuffle=True):
+def eimuToLSTMInput(eimuPath : str, shuffle=True) -> tuple[tuple, tuple]:
     session = SessionData.fromPath(eimuPath)
     data, timestamps = session.toRaw()
     predicates = np.array(list(map(timestampPredicate, timestamps)))
@@ -30,14 +25,18 @@ def eimuToLSTMInput(eimuPath, shuffle=True):
     return (data[:trainLength], predicates[:trainLength]), (data[trainLength:], predicates[trainLength:])
 
 # convert a directory of eimu files to a tuple of (trainX, trainY), (testX, testY)
-def directoryToLSTMInput(path):
+def directoryToLSTMInput(path : str) -> tuple[tuple, tuple]:
     inputs = [eimuToLSTMInput(join(path,f)) for f in listdir(path) if isfile(join(path, f))]
     # combine all the inputs. each is a tuple of (trainX, trainY), (testX, testY),
     # and the result is a combination of all the trainX, trainY, testX, testY individually
     return (np.concatenate(list(map(lambda x: x[0][0], inputs))), np.concatenate(list(map(lambda x: x[0][1], inputs)))), (np.concatenate(list(map(lambda x: x[1][0], inputs))), np.concatenate(list(map(lambda x: x[1][1], inputs))))
 
 if __name__ == "__main__":
-    # (trainX, trainY), (testX, testY) = eimuToLSTMInput("./yawnn/data/long1.eimu")
+    model = Sequential()
+    model.add(LSTM(units=10, return_sequences=True))
+    model.add(Dense(units=1, activation='sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    
     (trainX, trainY), (testX, testY) = directoryToLSTMInput("./yawnn/data")
     model.fit(trainX, trainY, epochs=100, batch_size=32)
     model.evaluate(testX, testY)
