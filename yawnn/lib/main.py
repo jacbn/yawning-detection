c10677a02ed1e38df7b603a922dce3b61980e6d0
jacbn <jabrown405@gmail.com>
2023-01-08 16:27:27 +0000

    variable training model

--- yawnn/lib/main.py
+++ yawnn/lib/main.py
@@ -5,19 +5,22 @@ from eimuLSTM import EimuLSTMInput
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 
-# todo: make an LSTM class that has a variable model
-def makeLSTM(modelType : commons.ModelType, dataDirectory : str, lstmUnits : int, epochs : int, shuffle=True, equalPositiveAndNegative=True):
+def makeLSTMDense(lstmUnits : int):
     model = Sequential()
     model.add(LSTM(units=lstmUnits, return_sequences=True))
     model.add(Dense(units=1, activation='sigmoid'))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
 
+
+def trainModel(modelType : commons.ModelType, dataDirectory : str, model, epochs : int, shuffle=True, equalPositiveAndNegative=True):
     (trainX, trainY), (testX, testY) = commons.directoryToModelData(dataDirectory, modelType, shuffle=shuffle, equalPositiveAndNegative=equalPositiveAndNegative)
-
+    
     model.fit(trainX, trainY, epochs=epochs, batch_size=32)
     model.evaluate(testX, testY)
     
+    
 if __name__ == "__main__":
-    # makeLSTM(EimuLSTMInput(), "./yawnn/data/96hz", 10, 100)
-    makeLSTM(FourierLSTMInput(), "./yawnn/data/96hz", lstmUnits=10, epochs=100)
+    # trainModel(EimuLSTMInput(), makeLSTMDense(lstmUnits=10), "./yawnn/data/96hz", epochs=100)
+    trainModel(FourierLSTMInput(), makeLSTMDense(lstmUnits=10), "./yawnn/data/96hz", epochs=100)
     
