2c2f9b33224ccbbb8896d89c0b0b843345151288
jacbn <jabrown405@gmail.com>
2023-04-08 00:29:56 +0100

    typing fix

--- yawnn/yawnnlib/utils/config.py
+++ yawnn/yawnnlib/utils/config.py
@@ -1,4 +1,5 @@
 from os.path import join, abspath
+from typing import Any
 import yaml
 
 PROJECT_ROOT = abspath(join(__file__, '../../..')) # yawning-detection/yawnn/
@@ -6,7 +7,7 @@ CONFIG_PATH = f"{PROJECT_ROOT}/yawnnlib/config.yaml"
 file = open(CONFIG_PATH, "r")
 config = yaml.load(file, Loader=yaml.FullLoader)
 
-def get(key : str):
+def get(key : str) -> Any:
     result = config[key]
     if type(result) == str:
         # replace strings with {PROJECT_ROOT} with the actual path
